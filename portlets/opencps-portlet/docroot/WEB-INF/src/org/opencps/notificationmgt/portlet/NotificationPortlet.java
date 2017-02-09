634a0eda8ab9bcd3f5596d1444210fdfc3cdad28
Nhan Hoang <skynetx002@gmail.com>
2017-02-09 15:54:06 +0700

    Thêm hàm cập nhật notification config

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/notificationmgt/portlet/NotificationPortlet.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/notificationmgt/portlet/NotificationPortlet.java
@@ -18,17 +18,24 @@
 package org.opencps.notificationmgt.portlet;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletException;
 
-import org.opencps.util.SendMailUtils;
+import org.opencps.notificationmgt.model.NotificationConfig;
+import org.opencps.notificationmgt.search.NoficationConfigDisplayTerms;
+import org.opencps.notificationmgt.service.NotificationConfigLocalServiceUtil;
+import org.opencps.util.WebKeys;
 
-import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 /**
  * @author nhanhoang
@@ -36,12 +43,79 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
 public class NotificationPortlet extends MVCPortlet {
 
 	private static Log _log = LogFactoryUtil.getLog(NotificationPortlet.class);
+	
+	public void updateNotificationConfig(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException {
 
-	public void sendUserNotification(ActionRequest actionRequest, ActionResponse actionResponse)
-		throws IOException, PortletException, PortalException, SystemException {
-		
+		long notificationConfigId = ParamUtil.getLong(actionRequest,
+				NoficationConfigDisplayTerms.NOTIFICATIONCONFIG_ID);
+		String dossierCurrentStatus = ParamUtil.getString(actionRequest,
+				NoficationConfigDisplayTerms.DOSSIER_CURRENT_STATUS);
+		String dossierNextStatus = ParamUtil.getString(actionRequest,
+				NoficationConfigDisplayTerms.DOSSIER_NEXT_STATUS);
+		boolean isSendNotification = ParamUtil.getBoolean(actionRequest,
+				NoficationConfigDisplayTerms.IS_SEND_NOTIFICATION);
 
-		//SendMailUtils.sendEmail("no-reply@fds.vn", "hltn.works@gmail.com", "skynetx001@gmail.com,nhanhlt@fds.vn", "subject", "body", true);
+		String returnURL = ParamUtil.getString(actionRequest,
+				WebKeys.RETURN_URL);
+		String currentURL = ParamUtil.getString(actionRequest,
+				WebKeys.CURRENT_URL);
+		String backURL = ParamUtil.getString(actionRequest, WebKeys.BACK_URL);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest
+				.getAttribute(WebKeys.THEME_DISPLAY);
+
+		NotificationConfig notificationConfig = null;
+
+		try {
+
+			try {
+				notificationConfig = NotificationConfigLocalServiceUtil
+						.fetchNotificationConfig(notificationConfigId);
+			} catch (SystemException e) {
+
+			}
+
+			if (Validator.isNull(notificationConfig)) {
+
+				notificationConfig = NotificationConfigLocalServiceUtil
+						.createNotificationConfig(CounterLocalServiceUtil
+								.increment(NotificationConfig.class.getName()));
+
+				notificationConfig.setCompanyId(themeDisplay.getCompanyId());
+				notificationConfig.setGroupId(themeDisplay.getScopeGroupId());
+				notificationConfig.setUserId(themeDisplay.getUserId());
+				notificationConfig.setCreateDate(new Date());
+				notificationConfig.setModifiedDate(new Date());
+
+				notificationConfig
+						.setDossierCurrentStatus(dossierCurrentStatus);
+				notificationConfig.setDossierNextStatus(dossierNextStatus);
+				notificationConfig.setIsSendNotification(isSendNotification);
+
+				NotificationConfigLocalServiceUtil
+						.addNotificationConfig(notificationConfig);
+
+			} else {
+
+				notificationConfig.setModifiedDate(new Date());
+
+				notificationConfig
+						.setDossierCurrentStatus(dossierCurrentStatus);
+				notificationConfig.setDossierNextStatus(dossierNextStatus);
+				notificationConfig.setIsSendNotification(isSendNotification);
+
+				NotificationConfigLocalServiceUtil
+						.updateNotificationConfig(notificationConfig);
+
+			}
+			
+			SessionMessages.add(actionRequest, "update-notification-config-success");
+		} catch (Exception e) {
+			_log.error(e);
+			SessionMessages.add(actionRequest, "update-notification-config-error");
+		}
+
 	}
 
 }
